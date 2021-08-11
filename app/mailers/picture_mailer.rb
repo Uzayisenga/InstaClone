b26fb4ed321f2c152b8ff3ded4e8dbfe883b4901
taiki naya <taiki780528@gmail.com>
2021-08-11 11:24:33 +0000

    remove a debugging code

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -2,6 +2,5 @@ class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
     mail to: "@picture.user.email", subject: "Posting confirmation"
-    binding.pry
   end
 end
