39e5fcb0c620ac1d56ae90370a5379df294ea407
taiki naya <taiki780528@gmail.com>
2021-08-11 02:51:39 +0000

    changed config of email

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,6 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: "taiki780528@yahoo.co.jp", subject: "Posting confirmation"
+    mail to: "picture.user.email", subject: "Posting confirmation"
   end
 end
