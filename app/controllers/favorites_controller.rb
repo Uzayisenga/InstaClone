0922dfa3fce63e55b8724bba3fbb36a8baca6232
taiki naya <taiki780528@gmail.com>
2021-08-11 04:12:24 +0000

    implemented user favorited index page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,9 @@
 class FavoritesController < ApplicationController
+
+  def index
+    @favorites = current_user.favorites
+  end
+
   def create
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_path, notice: "#{favorite.picture.user.name}'s picture was added to your favorites"
